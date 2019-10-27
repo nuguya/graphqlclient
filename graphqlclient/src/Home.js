b318195cb90345b5c366bad5031bd55d2482f40a
Geunyoung Kim <emsud@inu.ac.kr>
2019-10-27 18:19:14 +0900

    @Add : Home.js Query 컴포넌트 추가하여 graphql로부터 데이터 받아오는 부분 구현
    Query 컴포넌트를 추가하고 render.prop을 통해 받아온 데이터에 대한 분기처리를 할 수 있는 로직 구현

--- graphqlclient/src/Home.js
+++ graphqlclient/src/Home.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Query } from "react-apollo";
+import { HOME_PAGE } from "./queries";
 
 const Home = () => {
-  return "Home";
+  return (
+    <Query query={HOME_PAGE}>
+      {({ loading, data, error }) => {
+        if (loading) return <span>loading</span>;
+        if (error) return <span>Error</span>;
+        return data.movies.map(movie => {
+          return (
+            <h2 key={movie.id}>
+              {movie.title} / {movie.rating}
+            </h2>
+          );
+        });
+      }}
+    </Query>
+  );
 };
 
 export default Home;
